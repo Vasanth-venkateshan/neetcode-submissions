c152ef1a70c6653fde607ccc607d7da70d9109df
Vasanth-venkateshan <vasanthven123@gmail.com>
2026-04-20 22:17:55 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+      HashMap<Character,Integer> map=new HashMap<>();
+      HashMap<Character,Integer> map2=new HashMap<>();
+      if(s.length()!=t.length())
+      {
+         return false;
+      }
+
+      for(int i=0;i<s.length();i++)
+      {
+         map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+      }
+      for(int i=0;i<t.length();i++)
+      {
+         map2.put(t.charAt(i),map2.getOrDefault(t.charAt(i),0)+1);
+      }
+
+      return map.equals(map2);
+
+
+    }
+}
+
+
